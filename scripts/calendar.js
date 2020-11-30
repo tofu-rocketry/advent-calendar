3d5fd673d1bac8ef03f343371cb027244dfe0b1f
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2020-11-30 16:35:05 +0000

    Customise how the calendar works and replace msgs

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -3,14 +3,17 @@ var currentDate = new Date();
 
 function Door(calendar, day) {
 
-	this.width = ((calendar.width - 0.1 * calendar.width) / 4) * 0.95;
-	this.height = ((calendar.height - 0.1 * calendar.height) / 6) * 0.95;
-	this.adventMessage = 'Day ' + day + ' of Advent\n\n' + '"' + messages[day - 1][0] + '"\n\n\t' + 'by ' + messages[day - 1][1] + '\n\n';
-	this.x = ( 0.04 * calendar.width + ((day- 1) % 4) * (1.1 * this.width) );
-	this.y = - ( 0.96 * calendar.height - Math.floor((day - 1) / 4) * (1.1 * this.height) );
+	this.width = ((calendar.width - 0.1 * calendar.width) / 6) * 0.95;
+	this.height = ((calendar.height - 0.1 * calendar.height) / 4) * 0.95;
 
-	this.content = function() { 
-		
+	this.adventURL = messages[day - 1][0];
+	this.adventMessage = 'Day ' + day + ' of Advent\n\n' + messages[day - 1][1] + '\n\n' + 'Click OK to open this YouTube video in a new tab.';
+
+	this.x = ( 0.04 * calendar.width + ((day- 1) % 6) * (1.1 * this.width) );
+	this.y = - ( 0.96 * calendar.height - Math.floor((day - 1) / 6) * (1.1 * this.height) );
+
+	this.content = function() {
+
 		var node = document.createElement("li");
 		document.getElementById("adventDoors").appendChild(node);
 		node.id = "door" + day;
@@ -28,11 +31,14 @@ function Door(calendar, day) {
 			}
 		} else {
 			var adventMessage = this.adventMessage;
-			innerNode.onclick = function() {
-				alert(adventMessage);
+			var adventURL = this.adventURL;
+						innerNode.onclick = function() {
+				if ( confirm(adventMessage)) {
+					open(adventURL);
+				};
 				return false;
 			}
-		}	
+		}
 	};
 
 }
