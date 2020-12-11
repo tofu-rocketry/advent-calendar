4b51f420d10bcb221b23ce0db985b8c089503e51
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2020-12-11 11:23:13 +0000

    Add vid for the 16th

--- scripts/messages.js
+++ scripts/messages.js
@@ -13,7 +13,7 @@ var messages = [
 	["https://www.youtube.com/watch?v=oafqdLk3dDo", "https://www.youtube.com/watch?v=oafqdLk3dDo"],
 	["https://www.youtube.com/watch?v=83tOO23C1jk", "Tram ride through the snow. (Bucharest)"],
 	["https://www.youtube.com/watch?v=_p1_ui4-LKU", "Defeat snow!! Shinkansen"],
-	["", ""],
+	["https://www.youtube.com/watch?v=MGszU8cA8_I", "British Transport Films - Making Tracks - 1956"],
 	["", ""],
 	["", ""],
 	["", ""],
